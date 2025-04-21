36955e014e65c12a80b9ce5cb2b9822bd91eae4b
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-04-21 15:48:17 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bookly/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'Features/splash/presentation/views/splash_view.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
