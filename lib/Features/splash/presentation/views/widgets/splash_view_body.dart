765f4b625394e36cf42bba2b9eb3f67cd2454017
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-04-30 15:45:33 +0300

    add navigate in splash view to to home view

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,37 @@
+import 'package:bookly/Features/home/presentation/views/home_view.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() =>
+      _SplashViewBodyState();
+}
+
+class _SplashViewBodyState
+    extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,17 +40,39 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment:
           CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
-        const SizedBox(height: 5),
-        const Text(
-          "Read Free Books",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
-          textAlign: TextAlign.center,
+        SlideTransition(
+          position: slidingAnimation,
+          child: Image.asset(AssetsData.logo),
         ),
       ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+    animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Get.to(
+          () => const HomeView(),
+          transition: Transition.fadeIn,
+          duration: kTransitionDuration,
+        );
+      },
     );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
+const kTransitionDuration = Duration(
+  milliseconds: 300,
+);
